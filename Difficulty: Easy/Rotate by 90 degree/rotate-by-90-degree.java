192d6c85ad95b7f3b01e995c40c440dfd6e90423
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2024-12-21 11:40:01 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int matrix[][] = new int[n][n];
+            // String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) System.out.print(matrix[i][j] + " ");
+                System.out.println();
+            }
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static void rotateby90(int matrix[][]) {
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each column of the transposed matrix
+        for (int j = 0; j < matrix.length; j++) {
+            for (int i = 0; i < matrix.length / 2; i++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - 1 - i][j];
+                matrix[matrix.length - 1 - i][j] = temp;
+            }
+        }
+    }
+}
