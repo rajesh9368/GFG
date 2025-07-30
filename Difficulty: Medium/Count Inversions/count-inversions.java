613f0233a319264d1cf5cf4b3a25431c4a7eff26
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2025-07-30 18:16:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -1,65 +1,38 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().inversionCount(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     static int inversionCount(int arr[]) {
-        return mergeSort(arr,0,arr.length-1);
+        return mergeSort(arr, 0, arr.length - 1);
     }
-    
-    private static int mergeSort(int[] arr,int left,int right){
-        int count=0;
-        if(left<right){
-            int mid=left+(right-left)/2;
-            count+=mergeSort(arr,left,mid);
-            count+=mergeSort(arr,mid+1,right);
-            count+=merge(arr,mid,left,right);
-        }
+
+    static int mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return 0;
+
+        int mid = (left + right) / 2;
+        int count = 0;
+
+        count += mergeSort(arr, left, mid);
+        count += mergeSort(arr, mid + 1, right);
+        count += merge(arr, left, mid, right);
+
         return count;
     }
-    private static int merge(int[] arr,int mid,int left,int right){
-        int l=left,r=mid+1;
-        int count=0;
-        ArrayList<Integer> temp=new ArrayList<>();
-        while(l<=mid && r<=right){
-            if(arr[l]<=arr[r]){
-                temp.add(arr[l]);
-                l++;
-            }else{
-                count+=(mid-l+1);
-                temp.add(arr[r]);
-                r++;
+    static int merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+        int invCount = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+                invCount += (mid - i + 1);
             }
         }
-        while(l<=mid){
-            temp.add(arr[l]);
-            l++;
+        while (i <= mid) temp[k++] = arr[i++];
+        while (j <= right) temp[k++] = arr[j++];
+        for (int p = 0; p < temp.length; p++) {
+            arr[left + p] = temp[p];
         }
-        while(r<=right){
-            temp.add(arr[r]);
-            r++;
-        }
-        for(int i=left;i<=right;i++){
-            arr[i]=temp.get(i-left);
-        }
-        return count;
+        return invCount;
     }
-}   
+}
