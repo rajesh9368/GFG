33f963ee2b3a86def5ceb768b95cd2800786c1af
Rajesh Kumar <123871464+rajesh9368@users.noreply.github.com>
2025-08-22 11:37:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Geek's Training/geeks-training.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    public int maximumPoints(int arr[][]) {
+        // code here
+        int n = arr.length;
+        int m = arr[0].length;
+        int[][] dp = new int[n][3];
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        for(int i=1;i<n;i++){
+            dp[i][0] = arr[i][0] + Math.max(dp[i-1][1],dp[i-1][2]);
+            dp[i][1] = arr[i][1] + Math.max(dp[i-1][0],dp[i-1][2]);
+            dp[i][2] = arr[i][2] + Math.max(dp[i-1][1],dp[i-1][0]);
+        }
+        return Math.max(dp[n-1][0],Math.max(dp[n-1][1],dp[n-1][2]));
+    }
+}
